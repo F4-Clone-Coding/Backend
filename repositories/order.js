31a331a34a95b3f1ceb9451ce4ea23527adfdeb4
(JongHyeonMoon) <(zin354@gmail.com)>
2022-10-28 21:36:47 +0900

    no message

--- repositories/order.js
+++ repositories/order.js
@@ -2,13 +2,13 @@ const { Order } = require("../models");
 
 class OrderRepository {
 
-findOrder = async ({ userId, storeId }) => {
+findOrder = async ( userId, storeId ) => {
     const findOneOrder = await Order.findOne({ where: { userId, storeId } });
     return findOneOrder;
   };
 
-  createOrder = async ({ userId, storeId }) => {
-    const createOrder = await Order.create({ userId, storeId });
+  createOrder = async ( userId, storeId ) => {
+    const createOrder = await Order.create( userId, storeId );
     return createOrder;
   };
 

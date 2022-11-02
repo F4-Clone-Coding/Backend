3bfc8f78a67bc4187a59ac36bc664049b1b59eef
김지헌 <youwa65@gmail.com>
2022-11-02 13:26:04 +0900

    스케줄

--- utils/resetview.js
+++ utils/resetview.js
@@ -4,14 +4,13 @@ const schedule = require('node-schedule');
 module.exports = {
   resetview: async () => {
     try {
-      console.log('초기화코드 12시간뒤 동작');
+      console.log('초기화코드 월요일 정각에 동작');
       // 1시간 마다 이메일 스케쥴 실행 "*/! * * * *" //1분마다 실행
-      schedule.scheduleJob({ hour: 12 }, async () => {
+      schedule.scheduleJob({ hour: 0, dayOfWeek: 1 }, async () => {
         await Store.update({ viewRecent: 0 }, { where: {} });
       });
     } catch (err) {
       console.log(err);
     }
   },
-  // await Store.update({ viewRecent: 0 }, { where: {} });
 };
